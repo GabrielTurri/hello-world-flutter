db9342b0b8c246d5c2d607c4414b19254e3dd8a2
Gabriel Turri <ga-briel.t.a@hotmail.com>
2024-02-14 19:51:19 -0300

    adding navigation rail

--- lib/main.dart
+++ lib/main.dart
@@ -58,42 +58,76 @@ class MyAppState extends ChangeNotifier {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          SafeArea(
+            child: NavigationRail(
+              extended: false,
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text('Home'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.favorite),
+                  label: Text('Favorites'),
+                ),
+              ],
+              selectedIndex: 0,
+              onDestinationSelected: (value) {
+                print('selected: $value');
+              },
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: GeneratorPage(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BigCard(pair: pair),
-            SizedBox(height: 10),
-            Row(
-              // mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ElevatedButton.icon(
-                    onPressed: () {
-                      appState.toggleFavorite();
-                    },
-                    label: Text('Like'),
-                    icon: Icon(
-                      appState.isFavorited
-                          ? Icons.favorite
-                          : Icons.favorite_border,
-                      size: 16,
-                    )),
-                SizedBox(width: 10),
-                ElevatedButton(
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
                   onPressed: () {
-                    appState.getNext();
+                    appState.toggleFavorite();
                   },
-                  child: Text('Next'),
-                ),
-              ],
-            )
-          ],
-        ),
+                  label: Text('Like'),
+                  icon: Icon(
+                    appState.isFavorited
+                        ? Icons.favorite
+                        : Icons.favorite_border,
+                    size: 16,
+                  )),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
